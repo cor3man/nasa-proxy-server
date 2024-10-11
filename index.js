0405e53770a3d6d833594a471c5f0231fe5a1d7c
Aliaksandr Lisotski <aliaksandr.lisotski@godel.shellenergy.co.uk>
2024-10-11 13:05:50 +0200

    Task-3_Add_environment_variables

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const axios = require('axios');
 const { format, addDays, startOfWeek, endOfWeek } = require('date-fns');
-
+require('dotenv').config();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-const NASA_API_KEY = '3wnO1v2VLBwGIzc05o6yoOdKWc0X0qi9obIDm5gG';
+
+const NASA_API_KEY = process.env.NASA_API_KEY;
+const NASA_API_NEO_URL = process.env.NASA_API_NEO_URL;
+
+if (!NASA_API_KEY || !NASA_API_NEO_URL) {
+  console.error('Cant read env variables');
+  process.exit(1); 
+}
+
 
 const today = new Date();
 const startDate = startOfWeek(today, { weekStartsOn: 1 });
@@ -12,10 +20,13 @@ const endDate = addDays(startDate, 4);
 const formattedStartDate = format(startDate, 'yyyy-MM-dd');
 const formattedEndDate = format(endDate, 'yyyy-MM-dd');
 
-const nasaUrl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`;
+const nasaUrl = `${NASA_API_NEO_URL}?start_date=${formattedStartDate}&end_date=${formattedEndDate}&api_key=${NASA_API_KEY}`;
+
+
 
 async function getAsteroids() {
     try {
+      console.log(nasaUrl)
       const response = await axios.get(nasaUrl);
       console.log('Response:', response.data);
     } catch (error) {
@@ -23,4 +34,4 @@ async function getAsteroids() {
     }
   }
   
-  getAsteroids();
+getAsteroids();
