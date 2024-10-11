757289cec63cbd011c3a22feefb1b89b5acb1f80
Aliaksandr Lisotski <aliaksandr.lisotski@godel.shellenergy.co.uk>
2024-10-11 15:53:29 +0200

    Task-7_Add_logic_to_your_app

--- controller/meteorsController.js
+++ controller/meteorsController.js
@@ -3,13 +3,37 @@ const { format, addDays, startOfWeek, endOfWeek } = require('date-fns');
 
 const getMeteors = async (req, res) => {
   try {
-    const response = await axios.get(`${process.env.NASA_API_NEO_URL}?start_date=${period().start}&end_date=${period().end}&api_key=${process.env.NASA_API_KEY}`);
-    res.json(response.data);
+    const url = `${process.env.NASA_API_NEO_URL}?start_date=${period().start}&end_date=${period().end}&api_key=${process.env.NASA_API_KEY}`;
+    console.debug(`Requested url: ${url}`)
+
+    const response = await axios.get(url);
+
+    res.json(exctractEssentialData(response));
+
   } catch (error) {
     console.error('Error while making the request:', error.message);
     res.status(500).send('Error while fetching data from NASA API');
   }
 };
+
+
+function exctractEssentialData(response) {
+  const asteroids = response.data.near_earth_objects;
+
+  const filteredAsteroids = Object.values(asteroids).flatMap(day => 
+    day.map(asteroid => ({
+      id: asteroid.id,
+      name: asteroid.name,
+      diameter: asteroid.estimated_diameter.meters,
+      is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+      close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+      relative_velocity: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+    }))
+  );
+
+  return filteredAsteroids;
+}
+
 
 function period() {
   const today = new Date();
